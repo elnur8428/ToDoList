30f87f0f5e54c0bd6c57a96c9db8de207ea8bf8b
Elnur <ocaqquliyev8428@gmail.com>
2022-06-08 23:12:51 +0400

    First Comment

---
+++ main.js
@@ -0,0 +1,22 @@
+function TodoList(id,title,date,isDone){
+    this.id = id;
+    this.title = title;
+    this.date = date;
+    this.isDone = isDone;
+}
+
+
+function addList = ()=>{
+
+const list = [new TodoList(123,"Name",new Date, "isDone")];
+console.log(addList); 
+    
+};
+
+
+
+
+
+
+
+
