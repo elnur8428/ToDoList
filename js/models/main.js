d0238f42853417c7752acf578c2828c6a06d3a2c
Elnur <ocaqquliyev8428@gmail.com>
2022-06-17 15:09:20 +0400

    Second Commit

---
+++ js/models/main.js
@@ -0,0 +1,17 @@
+(function Add(){
+    const todo = [
+        new Todo(1, "title 1", new Date(), "isDone"),
+        new Todo(2, "title 2", new Date(), "isDone"),   
+        new Todo(3, "title 3", new Date(), "isDone")
+]
+    console.log(todo);
+})();
+
+
+
+
+
+
+
+
+
---
+++ js/models/todo.js
@@ -0,0 +1,6 @@
+function Todo(id, title, date, isDone){
+    this.id = id;
+    this.title = title;
+    this.date = date;
+    this.isDone = isDone;
+}
